84ac68d30828232636a25f92e5bfbbb68ead4de9
inaki beroiz dominguez <antxonito_92@hotmail.com>
2014-05-27 18:19:04 +0200

    Script MyEditor

---
+++ Assets/Editor/MyEditor.cs
@@ -0,0 +1,42 @@
+using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+class MyEditorScript {
+        static string[] SCENES = FindEnabledEditorScenes();
+
+        static string APP_NAME = "Elements";
+        static string TARGET_DIR = "C:/Proyectos";
+
+        [MenuItem ("Custom/Build Android")]
+        static void PerformAndroidBuild ()
+        {
+                 string target_dir = APP_NAME + ".apk";
+                 GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.Android, BuildOptions.None);
+        }
+        private static string[] FindEnabledEditorScenes(){
+		List<string> EditorScenes = new List<string>();
+		foreach(EditorBuildSettingsScene scene in EditorBuildSettings.scenes) {
+			if (!scene.enabled) continue;
+			EditorScenes.Add(scene.path);
+		}
+		return EditorScenes.ToArray();
+	}
+
+        static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
+        {
+                EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
+                string res = BuildPipeline.BuildPlayer(scenes,target_dir,build_target,build_options);
+                if (res.Length > 0) {
+                        throw new Exception("BuildPlayer failure: " + res);
+                }
+        }
+}
+
+public static class AndroidSDKFolder{
+    public static string Path{
+        get{return EditorPrefs.GetString("AndroidSdkRoot");}
+        set{EditorPrefs.SetString("AndroidSdkRoot", "C:/adt-bundle-windows-x86_64-20131030/sdk");}
+    }
+}
